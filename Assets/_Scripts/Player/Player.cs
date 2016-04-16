767db3fab56737f9330586e674d9c093ccc6f8fe
pasketi <pasketi@gmail.com>
2016-04-16 11:54:20 +0300

    Player commit

---
+++ Assets/_Scripts/Player/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] private float velocity = 10f;
+	private Rigidbody2D rigidBody;
+	private Vector2 inputData;
+	private Transform muzzle;
+
+	public BaseHealth health;
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	public virtual void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		muzzle = transform.FindChild ("Muzzle");
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		inputData = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		move (inputData);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			shoot ();
+	}
+
+	private void move(Vector2 movement) {
+		Debug.Log("Mag " + movement.magnitude);
+		Debug.Log("Norm " + movement.normalized);
+		rigidBody.velocity = movement.normalized * velocity;
+		Debug.Log("Vel " + rigidBody.velocity + " " + rigidBody.velocity.magnitude);
+	}
+
+	private void shoot() {
+		Instantiate (bulletPrefab, muzzle.position, Quaternion.identity);
+	}
+}
---
+++ Assets/_Scripts/bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+	[SerializeField] private float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, speed);
+	}
+}
