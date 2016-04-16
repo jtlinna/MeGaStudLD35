30f48c842b66501fb8762c84bd7a0b5d3f78d753
jtlinna <jukka.linna@gmail.com>
2016-04-16 10:51:04 +0300

    Created scrolling background

--- Assets/_Scripts/ScrollingBackground.cs
+++ Assets/_Scripts/ScrollingBackground.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Renderer))]
 public class ScrollingBackground : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    private float ScrollSpeed = 0.25f;
+
+    private float _scrollSpeed;
+
+    private Material _backgroundMaterial;
+
+    void Awake()
+    {
+        _backgroundMaterial = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        if(_backgroundMaterial != null)
+        {
+            Vector2 offset = _backgroundMaterial.mainTextureOffset;
+            offset.y += (ScrollSpeed / 100f * Time.deltaTime); // Just to get a reasonable scale for the speed
+            _backgroundMaterial.mainTextureOffset = offset;
+        }
+    }
 }
