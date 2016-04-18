756bd6b07f94422724809bac76671a146062083c
jtlinna <jukka.linna@gmail.com>
2016-04-19 02:04:22 +0300

    asdsadsad

--- Assets/_Scripts/Player/PowerUp.cs
+++ Assets/_Scripts/Player/PowerUp.cs
@@ -39,6 +39,7 @@ public class PowerUp : MonoBehaviour {
 	protected GameManager manager;
 	[SerializeField] private float MoveSpeed = 3f;
 
+    private Transform _player;
     void Awake()
     {
         Add(this);
@@ -46,6 +47,11 @@ public class PowerUp : MonoBehaviour {
 
 	void Start () {
 		manager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        if (go != null)
+        {
+            _player = go.transform;
+        }
 	}
 
     void OnDestroy()
@@ -56,6 +62,16 @@ public class PowerUp : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position += -transform.up.normalized * MoveSpeed * Time.deltaTime;
+        Vector3 offset = Vector3.zero;
+
+        if (_player != null)
+        {
+            if (_player.position.x <= transform.position.x)
+                offset = -transform.right;
+            else
+                offset = transform.right;
+        }
+        transform.position += offset * Time.deltaTime * 0.1f;
 	}
 
 	public virtual void OnTriggerEnter2D (Collider2D other) {
