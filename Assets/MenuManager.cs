03805a1a767e7b6c85b0747f74fe853e1ea33ce2
pasketi <pasketi@gmail.com>
2016-04-18 22:52:32 +0300

    MainMenu

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
+	private Button Play, Tutorial, Quit, Back;
+	public GameObject TutorialImg;
+
+	public string GameSceneName;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		Play = GameObject.Find ("Play").GetComponent<Button> ();
+		Tutorial = GameObject.Find ("HowToPlay").GetComponent<Button> ();
+		Quit = GameObject.Find ("Quit").GetComponent<Button> ();
+		Back = GameObject.Find ("Back").GetComponent<Button> ();
+		TutorialImg.SetActive (false);
+
+		Play.onClick.AddListener (() => loadGame());
+		Tutorial.onClick.AddListener (() => openTutorialPanel());
+		Quit.onClick.AddListener (() => quitGame());
+		Back.onClick.AddListener (() => closeTutorialPanel());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void loadGame () {
+		SceneManager.LoadScene (GameSceneName); 
+	}
+
+	private void openTutorialPanel () {
+		TutorialImg.SetActive(true);
+	}
+
+	private void closeTutorialPanel () {
+		TutorialImg.SetActive(false);
+	}
+
+	private void quitGame () {
+		Application.Quit();
 	}
 }
