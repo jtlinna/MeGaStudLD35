426413a5f08ed41ea82a7be6aa972cdae4ab9615
pasketi <pasketi@gmail.com>
2016-04-18 15:14:55 +0300

    BOOBDS

--- Assets/_Scripts/Enemies/BossScript.cs
+++ Assets/_Scripts/Enemies/BossScript.cs
@@ -16,22 +16,36 @@ public class BossScript : MonoBehaviour {
 	public Collider2D bossCollider;
 	public static System.Action OnBossDied;
 	private bool thirdPhaseDead = false;
-	private float spawnerPositions = 0f;
+	private float spawnerPositions = 1f;
+	public float bossSpeed = 8f;
+	public bool bossGoRight = true;
+
+	private Transform wpDefault, wpMiddle, wpTopLeft, wpTopRight, wpSpawn;
 
 	private float sequenceTimer = 0f;
 
 	private float coreSpinSpeed = 10f;
+	private float coreSpinSpeedDefault;
 
-	public int[] shotAmountOne, shotAmountTwo, shotAmountFour, shotAmountFive;
-	public float[] sequenceTimeOne;
+	public int[] shotAmountOne, shotAmountTwo, shotAmountFour;
+	public float[] sequenceTimeOne, sequenceTimeTwo;
+	public int[] tripAmout;
+	private int tripCounter;
 
-	private Transform[] sOneSpawns, sTwoSpawns, sThreeSpawns, sFourSpawns, sFiveSpawns;
+	private Transform[] sOneSpawns, sTwoSpawns, sThreeSpawn, sFourSpawns, sFiveSpawn;
 
 	//public int attackSequence;
 	private bool sequenceDone = false;
 
 	// Use this for initialization
 	void Awake () {
+		wpDefault = GameObject.Find("wpDefault").transform;
+		wpMiddle = GameObject.Find("wpMiddle").transform;
+		wpTopLeft = GameObject.Find("wpTopLeft").transform;
+		wpTopRight = GameObject.Find("wpTopRight").transform;
+		wpSpawn = GameObject.Find("wpSpawn").transform;
+
+		coreSpinSpeedDefault = coreSpinSpeed;
 		bossCollider = GetComponent<Collider2D> ();
 		bossCollider.enabled = false;
 
@@ -42,6 +56,8 @@ public class BossScript : MonoBehaviour {
 		_parts [2] = transform.GetChild (2);
 
 		sOneSpawns = new Transform[shotAmountOne[shotAmountOne.Length - 1]];
+		sThreeSpawn = new Transform[1];
+		sFiveSpawn = new Transform[1];
 
         if (AutoStart)
             StartCoroutine(DelayedInit());
@@ -58,6 +74,11 @@ public class BossScript : MonoBehaviour {
 			} else
 				sOneSpawns [i].gameObject.SetActive (false);
 		}
+
+		sThreeSpawn[0] = transform.FindChild ("SequenceThreeNFive");
+
+		sThreeSpawn[0].rotation = Quaternion.Euler(new Vector3(0f,0f,180f));
+
 		GetComponent<Animator> ().SetInteger ("phase", (int)phase);
 		StartCoroutine("preBossSequence", phase);
         Debug.Log(phase.ToString());
@@ -75,13 +96,9 @@ public class BossScript : MonoBehaviour {
 		_parts [2].Rotate (new Vector3(0f,0f, baseSpeed * Time.deltaTime));
 	}
 
-	private void moveBoss (float xPerSec, float yPerSec) {
-		transform.position = new Vector2 (transform.position.x + (xPerSec * Time.deltaTime), transform.position.y + (yPerSec * Time.deltaTime));
-	}
-
 	public IEnumerator preBossSequence (BossPhase phase) {
-		while (transform.position.y > 20f) {
-			moveBoss (0f, -2f);
+		while (transform.position != wpDefault.position) {
+			transform.position = Vector2.MoveTowards (transform.position, wpDefault.position, (bossSpeed * 0.25f) * Time.deltaTime);
 			yield return new WaitForEndOfFrame ();
 		}
 		bossCollider.enabled = true;
@@ -89,15 +106,16 @@ public class BossScript : MonoBehaviour {
 		Debug.Log ("PreSequenceDone");
 		yield break;
 	}
+
 	public IEnumerator sequenceOne (BossPhase phase) {
 
-		coreSpinSpeed = coreSpinSpeed * 3f;
-		sequenceTimer = 0f;
-
-		if (transform.position != new Vector3 (0f, 20f)) {
-			//Move me better bitch
-			transform.position = new Vector2 (0f, 20f);
+		while (transform.position != wpDefault.position) {
+			transform.position = Vector2.MoveTowards (transform.position, wpDefault.position, bossSpeed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
 		}
+
+		coreSpinSpeed = coreSpinSpeedDefault * 3f;
+		sequenceTimer = 0f;
 
 		bulletManager.StartCoroutine (bulletManager.spawnBullets(sOneSpawns, BulletSequenceManager.bulletIdentifier.octaBullet, 1, 0f, 0f, 1f/6f));
 
@@ -107,7 +125,7 @@ public class BossScript : MonoBehaviour {
 
 		bulletManager.stopSpawning ();
 		
-		coreSpinSpeed = coreSpinSpeed / 3f;
+		coreSpinSpeed = coreSpinSpeedDefault;
 
 		Debug.Log ("SequenceOneDone");
 		yield break;
@@ -115,24 +133,105 @@ public class BossScript : MonoBehaviour {
 
 	public IEnumerator sequenceTwo (BossPhase phase) {
 		yield return sequenceOne (phase);
+
+		while (transform.position != wpMiddle.position) {
+			transform.position = Vector2.MoveTowards (transform.position, wpMiddle.position, bossSpeed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
+		}
+
+		sequenceTimer = 0f;
+
+//		while (sequenceTimeOne[(int)phase - 1] > sequenceTimer) {
+//			yield return new WaitForEndOfFrame ();
+//		}
+
 		yield return new WaitForSeconds (2f);
 		Debug.Log ("SequenceTwoDone");
 		yield break;
 	}
+
 	public IEnumerator sequenceThree (BossPhase phase) {
 		yield return sequenceTwo (phase);
+
+		while (transform.position != wpTopLeft.position) {
+			transform.position = Vector2.MoveTowards (transform.position, wpTopLeft.position, bossSpeed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
+		}
+
+		tripCounter = 0;
+
+		bulletManager.StartCoroutine (bulletManager.spawnBullets(sThreeSpawn, BulletSequenceManager.bulletIdentifier.octaBullet, 1, 0f, 0f, 1f/10f));
+
+		while (tripAmout[(int)phase - 1] > tripCounter) {
+			if (bossGoRight)
+				transform.position = Vector2.MoveTowards (transform.position, wpTopRight.position, bossSpeed * 2f * Time.deltaTime);
+			else 
+				transform.position = Vector2.MoveTowards (transform.position, wpTopLeft.position, bossSpeed * 2f * Time.deltaTime);
+
+			if (transform.position == wpTopRight.position) {
+				bossGoRight = false;
+				tripCounter++;
+			}
+			if (transform.position == wpTopLeft.position){
+				bossGoRight = true;
+				tripCounter++;
+			}
+			
+			yield return new WaitForEndOfFrame ();
+		}
+
+		bulletManager.stopSpawning ();
+
 		yield return new WaitForSeconds (2f);
 		Debug.Log ("SequenceThreeDone");
 		yield break;
 	}
+
 	public IEnumerator sequenceFour (BossPhase phase) {
 		yield return sequenceThree (phase);
+
+		while (transform.position != wpDefault.position) {
+			transform.position = Vector2.MoveTowards (transform.position, wpDefault.position, bossSpeed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
+		}
+
 		yield return new WaitForSeconds (2f);
 		Debug.Log ("SequenceFourDone");
 		yield break;
 	}
+
 	public IEnumerator sequenceFive (BossPhase phase) {
 		yield return sequenceFour (phase);
+
+		while (transform.position != wpTopLeft.position) {
+			transform.position = Vector2.MoveTowards (transform.position, wpTopLeft.position, bossSpeed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
+		}
+
+		tripCounter = 0;
+
+		bulletManager.StartCoroutine (bulletManager.spawnBullets(sThreeSpawn, BulletSequenceManager.bulletIdentifier.triangleBullet, 1, 0f, 0f, 1f/10f));
+
+		while (tripAmout[(int)phase - 1] > tripCounter) {
+			if (bossGoRight)
+				transform.position = Vector2.MoveTowards (transform.position, wpTopRight.position, bossSpeed * 2f * Time.deltaTime);
+			else 
+				transform.position = Vector2.MoveTowards (transform.position, wpTopLeft.position, bossSpeed * 2f * Time.deltaTime);
+
+			if (transform.position == wpTopRight.position) {
+				bossGoRight = false;
+				tripCounter++;
+			}
+			if (transform.position == wpTopLeft.position){
+				bossGoRight = true;
+				tripCounter++;
+			}
+
+			yield return new WaitForEndOfFrame ();
+		}
+
+		bulletManager.stopSpawning ();
+
 		yield return new WaitForSeconds (2f);
 		Debug.Log ("SequenceFiveDone");
 		StartCoroutine ("sequenceFive", phase);
@@ -142,16 +241,8 @@ public class BossScript : MonoBehaviour {
 	public IEnumerator postBossSequence(BossPhase phase) {
 		bossCollider.enabled = false;
 		if (phase != BossPhase.third) {
-			bool bossZero = transform.position.x == 0f ? true : false;
-			bool bossLeft = transform.position.x < 0f ? true : false;
-			while (transform.position.y < 40f) {
-				if (!bossZero && (bossLeft && transform.position.x < 0f))
-					moveBoss (-1f, 0f);
-				else if (!bossZero && (!bossLeft && transform.position.x > 0f))
-					moveBoss (1f, 0f);
-				else
-					transform.position = new Vector2 (0f, transform.position.y);
-				moveBoss (0f, 2f);
+			while (transform.position != wpSpawn.position) {
+				transform.position = Vector2.MoveTowards (transform.position, wpSpawn.position, bossSpeed * 0.25f * Time.deltaTime);
 				yield return new WaitForEndOfFrame ();
 			}
 		} else {
