934445df7580a0e61aa77f5477becc2b779aa643
pasketi <pasketi@gmail.com>
2016-04-18 15:43:25 +0300

    Tissit

--- Assets/_Scripts/Bullets/DelayBullet.cs
+++ Assets/_Scripts/Bullets/DelayBullet.cs
@@ -8,12 +8,14 @@ public class DelayBullet : BaseBullet {
 	private float timer = 0f;
 	private float currentMoveSpeed;
 	private bool targetAquired = false;
+	private GameObject Player;
 
 	protected override void Awake() {
         base.Awake();
 		_movementVector = transform.up;
 		timer = 0f;
 		currentMoveSpeed = MoveSpeed;
+
 	}
 
     protected override void DoMovement()
@@ -26,8 +28,8 @@ public class DelayBullet : BaseBullet {
 			currentMoveSpeed = 0f;
 		} else if (timer >= (stopTime + timeToStop)) {
 			if (!targetAquired)
-				if (GameObject.Find ("Player") != null) 
-					SetTarget (GameObject.Find ("Player"));
+				if (!SetTarget("Player"))
+					SetTarget(GameObject.FindGameObjectWithTag ("Player"));
 			currentMoveSpeed = MoveSpeed * 2f;
 		}
 		transform.position += _movementVector * Time.deltaTime * currentMoveSpeed;
@@ -40,5 +42,16 @@ public class DelayBullet : BaseBullet {
 		_movementVector = target.transform.position - transform.position;
 		_movementVector = _movementVector.normalized;
 		targetAquired = true;
+	}
+
+	public bool SetTarget(string target)
+	{
+		if (GameObject.FindGameObjectWithTag (target) != null) {
+			_movementVector = GameObject.FindGameObjectWithTag (target).transform.position - transform.position;
+			_movementVector = _movementVector.normalized;
+			targetAquired = true;
+			return true;
+		} else
+			return false;
 	}
 }
